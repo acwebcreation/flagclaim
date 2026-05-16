1e3bd632cf9f37c0e8c4263be2c4a7fc2db07667
Alex <acwebcreation@gmail.com>
2026-05-16 10:41:56 +0200

    init projet flagclaim

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// js/config.js
+const SUPABASE_URL  = 'https://khxwzaqgayxzjtcvodqo.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoeHd6YXFnYXl4emp0Y3ZvZHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTExMDcsImV4cCI6MjA5NDQ2NzEwN30.lg36RWmCusXr5T5QX1kLUw21NkYYIUgXCwQ8shLXGY4'; // ta clé anon
